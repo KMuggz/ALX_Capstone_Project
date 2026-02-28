2ad53052ced8268f84e2bb49b5c0c69f554be109
KMuggz <karani.ivan25@gmail.com>
2026-02-28 13:03:27 +0300

    chore: debug logic added

--- moodmovie/_backend/_core/urls.py
+++ moodmovie/_backend/_core/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+# This tells Django to serve static files locally even if DEBUG is True
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
