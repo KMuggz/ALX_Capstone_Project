bc01147ac40b3071f686f79372822278652f4ea1
KMuggz <karani.ivan25@gmail.com>
2026-02-28 12:15:29 +0300

    fix: removes the double protocol

--- moodmovie/_frontend/src/api/axios.js
+++ moodmovie/_frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://https://mood-movie.netlify.app/',
+    baseURL: import.meta.env.VITE_API_URL || 'https://mood-movie.netlify.app/',
     withCredentials: true // Crucial for session-based feedback
 });
 
